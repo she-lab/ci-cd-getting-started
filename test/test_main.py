b5592205fb09d054c8db82a2a11d90663fa9ebfb
sheetalts <sheetalts025@gmail.com>
2025-06-15 14:36:59 +0530

    checking sub function

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,11 @@
-from src.main import add
+from src.main import add,sub
 
 def test_add_function():
     assert add(2,3)==5
-    assert add(0,0)==1
+    assert add(0,0)==0
     assert add(5,5)==10
+
+def test_sub_function():
+    assert sub(5,3)==2
+    assert sub(0,0)==0
+    assert sub(10,5)==5
